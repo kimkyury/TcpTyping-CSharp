aa4941704372b03d7e74894a862f683877694d0a
kimkyury <mandarining0918@gmail.com>
2021-06-22 18:25:10 +0900

    finish

--- TcpTyping-CSharp-Client/TcpTyping-CSharp-Client/Form1.Designer.cs
+++ TcpTyping-CSharp-Client/TcpTyping-CSharp-Client/Form1.Designer.cs
@@ -32,7 +32,6 @@ namespace TcpTyping_CSharp_Client
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -40,22 +39,23 @@ namespace TcpTyping_CSharp_Client
             this.label1 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Location = new System.Drawing.Point(45, 12);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "시작";
+            this.button2.Text = "게임시작";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(235, 311);
+            this.button3.Location = new System.Drawing.Point(235, 322);
             this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(66, 25);
@@ -66,7 +66,7 @@ namespace TcpTyping_CSharp_Client
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(67, 314);
+            this.textBox2.Location = new System.Drawing.Point(67, 325);
             this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(162, 21);
@@ -74,21 +74,10 @@ namespace TcpTyping_CSharp_Client
             this.textBox2.Text = "입력하세요";
             this.textBox2.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox2_KeyUp);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(293, 11);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(66, 23);
-            this.button1.TabIndex = 10;
-            this.button1.Text = "연결";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(10, 52);
+            this.label2.Location = new System.Drawing.Point(49, 89);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(29, 12);
             this.label2.TabIndex = 11;
@@ -98,9 +87,9 @@ namespace TcpTyping_CSharp_Client
             // 
             this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label3.Location = new System.Drawing.Point(66, 48);
+            this.label3.Location = new System.Drawing.Point(80, 85);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(106, 20);
+            this.label3.Size = new System.Drawing.Size(60, 20);
             this.label3.TabIndex = 12;
             this.label3.Text = "NULL";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -108,7 +97,7 @@ namespace TcpTyping_CSharp_Client
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(233, 52);
+            this.label4.Location = new System.Drawing.Point(233, 74);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(61, 12);
             this.label4.TabIndex = 13;
@@ -119,7 +108,7 @@ namespace TcpTyping_CSharp_Client
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.White;
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox1.Location = new System.Drawing.Point(45, 93);
+            this.pictureBox1.Location = new System.Drawing.Point(45, 118);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(278, 194);
             this.pictureBox1.TabIndex = 14;
@@ -128,16 +117,16 @@ namespace TcpTyping_CSharp_Client
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(93, 17);
+            this.label1.Location = new System.Drawing.Point(50, 40);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 12);
+            this.label1.Size = new System.Drawing.Size(67, 12);
             this.label1.TabIndex = 15;
-            this.label1.Text = "◀ 누르세요!";
+            this.label1.Text = "▲누르세요!";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(249, 67);
+            this.label5.Location = new System.Drawing.Point(249, 89);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(45, 12);
             this.label5.TabIndex = 16;
@@ -145,17 +134,30 @@ namespace TcpTyping_CSharp_Client
             // 
             // label6
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(219, 17);
+            this.label6.Location = new System.Drawing.Point(206, 13);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(0, 12);
+            this.label6.Size = new System.Drawing.Size(154, 16);
             this.label6.TabIndex = 17;
+            this.label6.Text = "미연결";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(235, 31);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "서버에 연결";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(371, 360);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label1);
@@ -163,7 +165,6 @@ namespace TcpTyping_CSharp_Client
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -183,7 +184,6 @@ namespace TcpTyping_CSharp_Client
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
@@ -191,6 +191,7 @@ namespace TcpTyping_CSharp_Client
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- TcpTyping-CSharp-Client/TcpTyping-CSharp-Client/Form1.cs
+++ TcpTyping-CSharp-Client/TcpTyping-CSharp-Client/Form1.cs
@@ -7,35 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Timer = System.Windows.Forms.Timer;
 
 namespace TcpTyping_CSharp_Client
 {
 
-    // 서버에서 올 단어를 담을 리스트 만들어야함
-    // 서버에서 올 속도를 담을 변수 만들어야함
-    // 클라이언트 서버 생각해야함 
-   
+    /* Server가 가진 정보를 요청하는 게임 실행 유저, client파일임 */
 
     public partial class Form1 : Form
     {
+        // 산성비에 필요한 변수
         private string str;
         private string current_str;
-     
-        //all_str 전체 문자수, correct_str 맞춘 개수
-        private int x, y, all_int, correct_int;
-        Random random = new Random();
+        private int x, y, all_int, correct_int;  // 좌표값들, 점수값들
+        Random random = new Random(); //위치설정
         Graphics drawString; // 문자가 나타날 바탕
         Brush b = new SolidBrush(Color.White); // 문자를 가리기 위한 도구
-        bool isStart; //게임시작
+        bool isStart; // 게임실행조건
 
 
-
-
-
-        // 통신을 통해 받아올 데이터
-        // 점찍기처럼 class로 담아올까? 암튼암튼
-        string[] stringList;
-        int listIndex;
+        // 통신관련 변수
+        TcpClient client;
+        Thread t1;
+        string speed;       // server에서 넘어온 speed
+        List<string> strList = new List<string>();  // server에서 온 제시어 여기다 붙일려고 했음.
+        int listIndex; // strList의 index
         
         public Form1()
         {
@@ -53,24 +52,79 @@ namespace TcpTyping_CSharp_Client
         private void Form1_Load(object sender, EventArgs e)
         {
             //타이머 뛸 때마다 다시 그리도록 작성한다
-            Timer T = new Timer();
+            System.Windows.Forms.Timer T = new Timer();
             T.Interval = 500;
             T.Tick += new EventHandler(Form1_Timer);
             T.Start();
         }
 
 
-        /* 연결버튼 */
+        /* 서버연결 */
         private void button1_Click(object sender, EventArgs e)
         {
-            label6.Text = "연결 성공";
-            set_stringList(); //일단 임시로 만든 string배열을 초기화시킴
+            try
+            {
+                client = new TcpClient("127.0.0.1", 8000);
+                client.Connect("127.0.0.1", 9000);
+                if (client.Connected)
+                {
+                    label6.Text = "소켓 연결";
+                    t1 = new Thread(new ThreadStart(recive)); //데이터 받아오는 함수 실행
+                    t1.IsBackground = true;
+                    t1.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                label6.Text = "소켓 미연결";
+            }
+       
+            //기능판독을 위하여 임시로 만든 string 리스트을 초기화시킴
+            set_strList("초기단어");
+            set_strList("기본단어");
+            set_strList("어쩌구저쩌구");
         }
 
-        /* 연결 성공시, 리스트의 내용을 받아옴 */
-        private void set_stringList()
+        /* Server데이터 받아오며 threadedWorks로 쓰이는 함수. */
+        private void recive()
         {
-            stringList = new string[] { "임시문자", "출력중이야", "몇개일까", "일곱개", "떨어진다", "짠짠", "와르르륵르" };
+            try
+            {
+                NetworkStream ns = client.GetStream();
+                Encoding ASCII = Encoding.ASCII;
+                Byte[] RecvBytes = new Byte[1024];
+
+                // **** 여기서 넘어온 데이터를 받아오는 거에서 막힘 
+                while (true)
+                {
+                    int bytes = ns.Read(RecvBytes, 0, RecvBytes.Length);
+                    if (client != null)
+                    {
+                        if (bytes == 0)
+                        {
+                            
+                        }
+                        else
+                        {
+                            set_strList(ASCII.GetString(RecvBytes, 0, bytes));
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ee) { }
+        }
+
+
+        /* 통신 정보 붙여넣기 위한 메소드 */
+        private void set_strList(string str)
+        {
+            strList.Add(str);
+            //stringList = new string[] { "임시문자", "출력중이야", "몇개일까", "일곱개", "떨어진다", "짠짠", "와르르륵르" };
+        }
+        private void set_speed(string speedbyte)
+        {
+            speed = speedbyte; 
         }
 
 
@@ -107,16 +161,16 @@ namespace TcpTyping_CSharp_Client
             str = "" + current_str;
 
             // 아래는 맞춘 수와 총 글자수 출력시킴
-            e.Graphics.DrawString("" + all_int, Font, Brushes.Black, 296, 53);
-            e.Graphics.DrawString("" + correct_int, Font, Brushes.Black, 296, 68);
+            e.Graphics.DrawString("" + all_int, Font, Brushes.Black, 296, 75);
+            e.Graphics.DrawString("" + correct_int, Font, Brushes.Black, 296, 90);
         }
 
         /* 다음으로 보낼 문자 결정 메소드 */
         private void next_char()
         {
             all_int += 1;
-            listIndex = random.Next(0, stringList.Length);
-            current_str = stringList[listIndex];
+            listIndex = random.Next(0, strList.Count);
+            current_str = strList[listIndex];
             x = random.Next(0, 218);  // pictureBox의 가로 길이 - (5글자 이내의 크기)
 
             y = -30; // pictureBox에서 글자가 내릴 시작점
@@ -166,7 +220,7 @@ namespace TcpTyping_CSharp_Client
             correct_int++;
             if(correct_int >= 5)
             {
-                MessageBox.Show("10개를 맞췄어요!", "종료", MessageBoxButtons.OK);
+                MessageBox.Show("5개를 맞췄어요!", "종료", MessageBoxButtons.OK);
                 Application.Exit();
             }
         }
